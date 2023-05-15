31545ae7d1e437080dad203cc65e31467bab704b
unknown <bijaykhadka722@gmail.com>
2023-05-16 04:30:43 +0545

    final changes

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import requests
+import json
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 # Load series data from CSV
 series_df = pd.read_csv('static/app.csv')
@@ -15,49 +16,76 @@ series_df.dropna(inplace=True)
 vectorizer = TfidfVectorizer()
 
 # Extract features from genres, titles, actors, and directors
-features = series_df['Genre']  + ' ' + series_df['Actors'] + ' ' + series_df['Director']+series_df['Tags']
+features = series_df['Genre'] + ' ' + series_df['Title'] + ' ' + series_df['Director'] + ' ' + series_df['Tags']
 feature_matrix = vectorizer.fit_transform(features)
 
-@app.route('/',methods=["POST"])
-def recommend():
-    data =requests.json()
-    names = data.get('name',[])
-    genres = data.get('genres',[])
-    tags = data.get('tags',[])
+@app.route('/', methods=['GET', 'POST'])
+def receive_data():
+    if request.method == 'POST':
+        data = request.get_json()
+
+        if data:
+            names = []
+            genres = []
+            tags = []
+
+            for show_data in data:
+                names.append(show_data.get('name', ''))
+                genres.extend(show_data.get('genres', []))  # Use extend instead of append
+                tags.append(show_data.get('tags', ''))
+                actors=  ['Joaquin Phoenix', 'Robert De Niro', 'Zazie Beetz', 'Frances Conroy']
 
 
+            user_preferences = {
+                'Genres': genres,
+                'Actors': actors,
+                'Titles': names,
+                'Tags': tags,
+            }
+
+            # Create user profile
+            user_profile = ' '.join(user_preferences['Genres'])  + \
+                           ' ' + ' '.join(user_preferences['Titles']) + ' ' + ' '.join(user_preferences['Tags'])
+            user_profile_matrix = vectorizer.transform([user_profile])
+
+            # Calculate similarity scores
+            similarity_scores = cosine_similarity(user_profile_matrix, feature_matrix)
+
+            # Get recommendations
+            recommendations = []
+            for i, score in enumerate(similarity_scores[0]):
+                series_title = series_df.iloc[i]['Title']
+                series_img = series_df.iloc[i]['Image']
+              
+                recommendations.append({'Title': series_title, 'Similarity Score': score, 'Images': series_img})
+
+            # Sort recommendations by similarity score
+            recommendations = sorted(recommendations, key=lambda x: x['Similarity Score'], reverse=True)
+           
+
+            data = recommendations[:10]
 
 
+            print("The top recommended Sereis are\n")
 
-    # User preferences (example)
-    user_preferences = {
-        'Genres': ' '.join(genres),
-        'Actors': ['Joaquin Phoenix', 'Robert De Niro', 'Zazie Beetz', 'Frances Conroy']
-,
-        'Director': ['José Esteban Alenda', 'César Esteban Alenda'],
-        'Tags': ' '.join(tags),
+            for i in data:
+                print(i['Title']);
+            
 
-    }
+            print("The similarity scores for recommended sereis are\n")
 
-    # Create user profile
-    user_profile = ' '.join(user_preferences['Genres']) + ' ' + ' '.join(user_preferences['Actors']) + ' ' + ' '.join(user_preferences['Director'])+ ' ' + ' '.join(user_preferences['Director'])
-    user_profile_matrix = vectorizer.transform([user_profile])
+            for i in data:
+                print (i['Similarity Score'])
+                
+                
 
-    # Calculate similarity scores
-    similarity_scores = cosine_similarity(user_profile_matrix, feature_matrix)
 
-    # Get recommendations 
-    recommendations = []
-    for i, score in enumerate(similarity_scores[0]):
-           series_title = series_df.iloc[i]['Title']
-           series_img = series_df.iloc[i]['Images']
-           recommendations.append({'Title': series_title, 'Similarity Score': score,'Images':series_img})
+            print (data)
 
-    # Sort recommendations by similarity score
-    recommendations = sorted(recommendations, key=lambda x: x['Similarity Score'], reverse=True)
+            # Return recommendations as JSON
+            return jsonify(recommendations[:10])
 
-    # Return recommendations as JSON
-    return jsonify(recommendations[:10])  # Return top 3 recommendations
+    return jsonify({'message': 'Invalid request'})
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     app.run(debug=True)
